39f1664728b8c2fd39bd6e74127114b1cee49df9
Richard Vizarreta <vizarretari27@byui.edu>
2021-11-18 19:46:01 -0700

    Richard, 11/18/2021 7:45 pm

--- app/src/main/java/com/example/termproject/ProfilePage.java
+++ app/src/main/java/com/example/termproject/ProfilePage.java
@@ -1,27 +1,18 @@
 package com.example.termproject;
-
 import android.os.Bundle;
-
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
 import android.view.View;
-
 import com.example.termproject.databinding.ActivityProfilePageBinding;
-
 public class ProfilePage extends AppCompatActivity {
 
     private ActivityProfilePageBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-
     }
 }
+
